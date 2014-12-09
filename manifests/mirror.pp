41be473775177ec5c5e6bae65de2bb9baecbc65a
Michael Chapman <woppin@gmail.com>
2014-12-09 23:52:05 +1100

    Add upstream cache option to mirror

--- manifests/mirror.pp
+++ manifests/mirror.pp
@@ -1,8 +1,19 @@
 class partial::mirror(
   $all_role          = 'all',
-  $repo_path         = '/usr/share/yumrepo'
+  $repo_path         = '/usr/share/yumrepo',
+  $upstream_cache    = false
 )
 {
+  # pull from a local copy to speed things up if possible
+  if $upstream_cache {
+    exec { 'pull_from_upstream_cache':
+      command => "cd ${repo_path}; wget -r -nH --cut-dirs=2 --no-parent --reject=\"index.html*\" http://${upstram_cache}/",
+      path    => ['/usr/bin', '/usr/local/bin','/usr/sbin','/sbin' ],
+      before  => Exec['build_repo'],
+      timeout => 0
+    }
+  }
+
   # this can take a very long time
   Yumrepo<||> ->
   exec { 'build_repo':
